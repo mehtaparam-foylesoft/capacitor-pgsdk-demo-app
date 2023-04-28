0b3fa2140c27bef388a7ba94303d48ff190cfed4
codingmantra <param@codingmantra.com>
2023-04-28 17:02:19 +0530

    Added Client ID and Secret in environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  cashFreeClientID: 'TEST37588254421bd6cc07cc5ac087288573',
+  cashFreeAppSecret: 'TESTd9993204b8259b7b54710fac440ac9f96d4de654'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  cashFreeClientID:'TEST37588254421bd6cc07cc5ac087288573',
+  cashFreeAppSecret: 'TESTd9993204b8259b7b54710fac440ac9f96d4de654'
 };
 
 /*
