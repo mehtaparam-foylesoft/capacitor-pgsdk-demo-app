5dc6eead2341ed6719c9ddbae45aeb257c07ca1c
codingmantra <param@codingmantra.com>
2023-04-28 17:03:42 +0530

    Added cashFreeClientID and cashFreeAppSecret in environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
+/* eslint-disable max-len */
 export const environment = {
   production: true,
-  cashFreeClientID: 'TEST37588254421bd6cc07cc5ac087288573',
-  cashFreeAppSecret: 'TESTd9993204b8259b7b54710fac440ac9f96d4de654'
+  cashFreeClientID: 'TEST37588254421bd6cc07cc5ac087288573', //Enter your appId here;
+  cashFreeAppSecret: 'TESTd9993204b8259b7b54710fac440ac9f96d4de654' //Enter your appSecret here (not to be used in your production app. use this in your backend api instead).
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
+/* eslint-disable max-len */
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
-  cashFreeClientID:'TEST37588254421bd6cc07cc5ac087288573',
-  cashFreeAppSecret: 'TESTd9993204b8259b7b54710fac440ac9f96d4de654'
+  cashFreeClientID: 'TEST37588254421bd6cc07cc5ac087288573', //Enter your appId here;
+  cashFreeAppSecret: 'TESTd9993204b8259b7b54710fac440ac9f96d4de654' //Enter your appSecret here (not to be used in your production app. use this in your backend api instead).
 };
 
 /*
